45438b53e52d5f08dcdf4f23efce91f3573ede79
Markus Lange <markus@gamedevteacher.com>
2023-09-04 22:29:25 +0200

    Fixed Tutorial

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public bool jumpPressed;
+    public bool jumpReleased;
+    public bool jumpHeld;
+
+    public Vector2 moveVector;
+
+    private void Update()
+    {
+        moveVector.x = (Keyboard.current.dKey.isPressed ? 1 : 0) + (Keyboard.current.aKey.isPressed ? -1 : 0);
+        moveVector.y = (Keyboard.current.wKey.isPressed ? 1 : 0) + (Keyboard.current.sKey.isPressed ? -1 : 0);
+        
+        jumpPressed = Keyboard.current.spaceKey.wasPressedThisFrame;
+        jumpReleased = Keyboard.current.spaceKey.wasReleasedThisFrame;
+        jumpHeld = Keyboard.current.spaceKey.isPressed;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,59 +1,40 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpSpeed = 7f;
-    
+
     public bool isPlayerGrounded;
     public LayerMask whatIsGround;
-
-    private Keyboard _keyboard;
-    private bool _jumpInput;
-    private bool _jumpedInput;
-    private bool _canJump;
-    private float _horizontalInput;
+    
     private Rigidbody2D _rigidbody2D;
+    private InputManager _input;
 
     private void Start()
     {
+
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _keyboard = Keyboard.current;
+        _input = GetComponent<InputManager>();
     }
 
     private void Update()
     {
         isPlayerGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.01f, whatIsGround);
         
-        UpdateInput();
-        
-        if (_jumpInput && isPlayerGrounded)
-        { _canJump = true; }
-
-        if (_jumpedInput && _rigidbody2D.velocity.y > 0f)
+        if (_input.jumpPressed && isPlayerGrounded)
         {
-            _rigidbody2D.velocity = new Vector2(
-                _rigidbody2D.velocity.x,
-                _rigidbody2D.velocity.y * 0.2f);
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpSpeed) * Time.deltaTime;
         }
         
+        if (_input.jumpReleased && _rigidbody2D.velocity.y > 0f)
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y * 0.2f) * Time.deltaTime;
+        }
     }
     
     private void FixedUpdate()
     {
-        _rigidbody2D.velocity = new Vector2(_horizontalInput * moveSpeed, _rigidbody2D.velocity.y);
-        if (_canJump)
-        {
-            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpSpeed);
-            _canJump = false;
-        }
-    }
-    private void UpdateInput()
-    {
-        _horizontalInput = (_keyboard.dKey.isPressed ? 1 : 0) + (_keyboard.aKey.isPressed ? -1 : 0);
-        _jumpInput = _keyboard.spaceKey.wasPressedThisFrame;
-        _jumpedInput = _keyboard.spaceKey.wasReleasedThisFrame;
-        // Interact, Climb, Crouch, Sprint, Glide, Dash, 
+        _rigidbody2D.velocity = new Vector2(_input.moveVector.x * moveSpeed, _rigidbody2D.velocity.y);
     }
 }
