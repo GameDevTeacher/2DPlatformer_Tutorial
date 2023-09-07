872003bfa3c9d9bcc59631421297a886c1745fef
Markus Lange <markus@hybrosia.com>
2023-09-07 14:30:27 +0200

    Tiles & Enemy Patrol p1

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -3,15 +3,34 @@ using UnityEngine;
 public class EnemyPatrol : MonoBehaviour
 {
     public float moveSpeed = 4f;
+    public LayerMask whatIsGround;
     private Rigidbody2D _rigidbody2D;
     
     private void Start()
-    {
-        _rigidbody2D = GetComponent<Rigidbody2D>(); }
-
+    { _rigidbody2D = GetComponent<Rigidbody2D>(); }
+    
     private void FixedUpdate()
+    { _rigidbody2D.velocity = new Vector2(
+        moveSpeed * transform.localScale.x, 
+        _rigidbody2D.velocity.y); 
+    }
+
+    private void LateUpdate()
     {
-        _rigidbody2D.velocity = new Vector2(
-            moveSpeed, 0f);
+        if (DetectedWallOrFall())
+        {
+            transform.localScale = new Vector3(
+                -transform.localScale.x, 1f, 1f);
+        }
+    }
+    
+    private bool DetectedWallOrFall()
+    {
+        // Origin, Direction, Distance, PhysicsLayer
+        return Physics2D.Raycast(
+            new Vector2(transform.position.x, 
+                transform.position.y + 0.2f),
+            Vector2.left * transform.localScale , 
+            0.6f, whatIsGround);
     }
 }
