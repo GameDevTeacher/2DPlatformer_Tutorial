96a985176c1ae044f04a6d171fcea2c328bc71e9
Markus Lange <markus@gamedevteacher.com>
2023-09-12 09:09:03 +0200

    Updated Input

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,19 +3,64 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    public bool jumpPressed;
-    public bool jumpReleased;
-    public bool jumpHeld;
-    
+   
     public Vector2 moveVector;
-    
+
+    [Header("Jumping")]
+    public bool canJump;
+    public bool jumpPressed, jumpReleased, jumpHeld;
+
+    [Header("Interacting")]
+    public bool canInteract;
+    public bool interactPressed, interactReleased, interactHeld;
+
+    private bool _usingGamepad;
+    private bool _usingDpad;
+        
+    private Keyboard _keyboard;
+    private Gamepad _gamepad;
+
+    private void Start()
+    {
+        _keyboard = Keyboard.current;
+        _gamepad = Gamepad.current;
+    }
+
     private void Update()
     {
-        moveVector.x = (Keyboard.current.dKey.isPressed ? 1 : 0) + (Keyboard.current.aKey.isPressed ? -1 : 0);
-        moveVector.y = (Keyboard.current.wKey.isPressed ? 1 : 0) + (Keyboard.current.sKey.isPressed ? -1 : 0);
-        
-        jumpPressed = Keyboard.current.spaceKey.wasPressedThisFrame;
-        jumpReleased = Keyboard.current.spaceKey.wasReleasedThisFrame;
-        jumpHeld = Keyboard.current.spaceKey.isPressed;
+        if (_usingGamepad)
+        {
+            UpdateGamepadInput();  
+        }
+        else
+        {
+            UpdateKeyboardInput();
+        }
+    }
+    
+    private void UpdateKeyboardInput()
+    {
+        moveVector.x = (_keyboard.dKey.isPressed ? 1 : 0) + (_keyboard.aKey.isPressed ? -1 : 0);
+        moveVector.y = (_keyboard.wKey.isPressed ? 1 : 0) + (_keyboard.sKey.isPressed ? -1 : 0);
+        jumpPressed = _keyboard.spaceKey.wasPressedThisFrame;
+        jumpReleased = _keyboard.spaceKey.wasReleasedThisFrame;
+        jumpHeld = _keyboard.spaceKey.isPressed;
+    }
+    
+    private void UpdateGamepadInput()
+    {
+        if (_usingDpad)
+        {
+            moveVector.x = (_gamepad.dpad.right.isPressed ? 1 : 0) + (_gamepad.dpad.right.isPressed ? -1 : 0);
+            moveVector.y = (_gamepad.dpad.right.isPressed ? 1 : 0) + (_gamepad.dpad.right.isPressed ? -1 : 0); 
+        }
+        else
+        {
+            moveVector = _gamepad.leftStick.ReadValue();
+        }
+    
+        jumpPressed = _gamepad.buttonSouth.wasPressedThisFrame;
+        jumpReleased = _gamepad.buttonSouth.wasReleasedThisFrame;
+        jumpHeld = _gamepad.buttonSouth.isPressed;
     }
 }
